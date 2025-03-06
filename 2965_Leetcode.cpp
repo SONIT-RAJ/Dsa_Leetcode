7105aebeaf36039700c905334ac6ece7fc9990ae
SONIT-RAJ <sonitraj8458@gmail.com>
2025-03-06 22:48:17 +0530

    add new question

---
+++ 2965_Leetcode.cpp
@@ -0,0 +1,37 @@
+/*
+
+Problem link - https://leetcode.com/problems/find-missing-and-repeated-values/description/?envType=daily-question&envId=2025-03-06
+
+Leetcode 2965 - Find Missing and Repeated Values
+
+Date- 06/03/2025
+
+TC - O(n*n)
+SC - O(n)
+
+*/
+
+class Solution {
+    public:
+        vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+            int sum=0;
+            int n=grid.size();
+            int a,b;
+            unordered_map<int,int>mpp;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    int r=grid[i][j];
+                    mpp[r]++;
+                    if(mpp[r]==2){
+                        a=r;
+                    }
+                    sum+=r;
+                }
+            }
+            int s=n*n;
+            int correctSum=(s*(s+1))/2;
+            b=correctSum+a-sum;
+            return {a,b};
+
+        }
+    };
