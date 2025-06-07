c1b579ce9396fc9442c1844568a6f4fffdd76295
SONIT-RAJ <sonitraj8458@gmail.com>
2025-06-07 09:49:29 +0530

    add new question

--- 2434_Leetcode.cpp
+++ 2434_Leetcode.cpp
@@ -9,3 +9,32 @@ TC - O(n)
 SC - O(n)
 
 */
+class Solution {
+public:
+    string robotWithString(string s) {
+        int n=s.size();
+        unordered_map<int,char>mpp;
+        char mini=s[n-1];
+        for(int i=n-2;i>=0;i--){
+            mpp[i]=mini;
+            mini=min(mini,s[i]);
+        }
+        string ans;
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            st.push(s[i]);
+            if(i==n-1){
+                break;
+            }
+            while(!(st.empty()) && st.top()<=mpp[i]){
+                ans.push_back(st.top());
+                st.pop();
+            }
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
