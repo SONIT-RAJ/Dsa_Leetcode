9f3901a8674b0a5a99789ca67be2d1bf936d9904
SONIT-RAJ <sonitraj8458@gmail.com>
2025-06-11 12:56:56 +0530

    add new question

---
+++ 3445_Leetcode.cpp
@@ -0,0 +1,13 @@
+/*
+
+Problem link - https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-ii/description/?envType=daily-question&envId=2025-06-11
+
+Leetcode 3445 - Maximum Difference Between Even and Odd Frequency II
+
+Date- 11/06/2025
+
+TC - O(n)
+SC - O(n)
+
+*/
+
